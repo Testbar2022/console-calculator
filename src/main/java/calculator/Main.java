00c1c537bb3d99c8cc223d112acd51255fd79cf3
Your Name <zhanna.prokopova.mail@gmail.com>
2023-04-11 09:02:33 +0200

    1

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -7,19 +7,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Pattern plusOppattern = Pattern.compile("\\s*(\\d+)\\s*\\+\\s*(\\d+)|\\s*\\+\\s*(\\d)|\\s+(\\d+)");
-        Pattern multiplyOppattern = Pattern.compile("\\s*(\\d+)\\s*\\*\\s*(\\d+)|\\s*\\*\\s*(\\d)|\\s+(\\d+)"); // multiplikation
+        Pattern plusOppattern = Pattern.compile("\\s*(\\d+)\\s*\\+\\s*(\\d+)|\\s*\\+\\s*(\\d)|\\s+(\\d+)");         // Addition
+        Pattern multiplyOppattern = Pattern.compile("\\s*(\\d+)\\s*\\*\\s*(\\d+)|\\s*\\*\\s*(\\d)|\\s+(\\d+)"); // Multiplikation
+        Pattern subtractOppattern = Pattern.compile("\\s*(\\d+)\\s*\\-\\s*(\\d+)|\\s*\\-\\s*(\\d)|\\s+(\\d+)"); // Subtraktion
+        Pattern divideOppattern = Pattern.compile("\\s*(\\d+)\\s*\\/\\s*(\\d+)|\\s*\\/\\s*(\\d)|\\s+(\\d+)");   // Division
+        Pattern moduloOppattern = Pattern.compile("\\s*(\\d+)\\s*\\%\\s*(\\d+)|\\s*\\%\\s*(\\d)|\\s+(\\d+)");   // Modulo
+
         Pattern quitPattern = Pattern.compile("^\\s*:q\\s*$");
+
         boolean calculatorStopped = false;
+
         Matcher quitMatcher;
         while (!calculatorStopped) {
             System.out.print("Eingabe tätigen: ");
             String input = scanner.nextLine();
             Matcher plusOpMatcher = plusOppattern.matcher(input);
-            Matcher multiplyOpMatcher = multiplyOppattern.matcher(input); //Multiplizeren
+            Matcher multiplyOpMatcher = multiplyOppattern.matcher(input);
+            Matcher subtractOpMatcher = subtractOppattern.matcher(input);
+            Matcher divideOpMatcher = divideOppattern.matcher(input);
+            Matcher moduloOpMatcher = moduloOppattern.matcher(input);
+
             quitMatcher = quitPattern.matcher(input);
 
-            if (plusOpMatcher.matches()) {
+            if (plusOpMatcher.matches()) {                               // Addition
                 if (plusOpMatcher.group(4) != null) {
                     double result = Double.parseDouble(plusOpMatcher.group(4));
                     System.out.println(input + " = " + result);
@@ -47,7 +57,50 @@ public class Main {
                     System.out.println(input + " = " + result);
                 }
             }
+            else if (subtractOpMatcher.matches()) {                              // Subtraktion
+                if (subtractOpMatcher.group(4) != null) {
+                    double result = Double.parseDouble(subtractOpMatcher.group(4));
+                    System.out.println(input + " = " + result);
+                } else if (subtractOpMatcher.group(3) != null) {
+                    double result = Double.parseDouble(subtractOpMatcher.group(3));
+                    System.out.println(input + " = " + result);
+                } else {
+                    double firstOperand = Double.parseDouble(subtractOpMatcher.group(1));
+                    double secondOperand = Double.parseDouble(subtractOpMatcher.group(2));
+                    double result = firstOperand - secondOperand;
+                    System.out.println(input + " = " + result);
+                }
+            }
 
+            else if (divideOpMatcher.matches()) {                                // Division
+                if (divideOpMatcher.group(4) != null) {
+                    double result = Double.parseDouble(divideOpMatcher.group(4));
+                    System.out.println(input + " = " + result);
+                } else if (divideOpMatcher.group(3) != null) {
+                    double result = Double.parseDouble(divideOpMatcher.group(3));
+                    System.out.println(input + " = " + result);
+                } else {
+                    double firstOperand = Double.parseDouble(divideOpMatcher.group(1));
+                    double secondOperand = Double.parseDouble(divideOpMatcher.group(2));
+                    double result = firstOperand / secondOperand;
+                    System.out.println(input + " = " + result);
+                }
+            }
+
+            else if (moduloOpMatcher.matches()) {                                // Modulo
+                if (moduloOpMatcher.group(4) != null) {
+                    double result = Double.parseDouble(moduloOpMatcher.group(4));
+                    System.out.println(input + " = " + result);
+                } else if (moduloOpMatcher.group(3) != null) {
+                    double result = Double.parseDouble(moduloOpMatcher.group(3));
+                    System.out.println(input + " = " + result);
+                } else {
+                    double firstOperand = Double.parseDouble(moduloOpMatcher.group(1));
+                    double secondOperand = Double.parseDouble(moduloOpMatcher.group(2));
+                    double result = firstOperand % secondOperand;
+                    System.out.println(input + " = " + result);
+                }
+            }
 
          else if (quitMatcher.matches()) {
             System.out.println("Taschenrechner wird gestopped...");
